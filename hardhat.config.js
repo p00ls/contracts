c1eb2197b21f2f0f356c20501a35d08f771cd58a
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-09-08 12:15:18 +0200

    use @amxx/hre

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,32 @@
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-ethers');
-require('@nomiclabs/hardhat-etherscan');
-require('@openzeppelin/hardhat-upgrades');
-require('hardhat-gas-reporter');
+require('dotenv/config');
 
 const argv = require('yargs/yargs')()
   .env('')
-  .string('mnemonic')
-  .string('fork')
-  .boolean('slow')
+  .options({
+    coverage:      { type: 'boolean',                                          default: false         },
+    report:        { type: 'boolean',                                          default: false         },
+    slow:          { type: 'boolean',                                          default: false         },
+    // compiler:      { type: 'string',                                           default: '0.8.7'       },
+    hardfork:      { type: 'string',                                           default: 'london'      },
+    mode:          { type: 'string', choices: [ 'production', 'development' ], default: 'development' },
+    fork:          { type: 'string',                                                                  },
+    coinmarketcap: { type: 'string'                                                                   },
+    etherscan:     { type: 'string'                                                                   },
+  })
   .argv;
+
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
+
+
+argv.coverage  && require('solidity-coverage');
+argv.etherscan && require('@nomiclabs/hardhat-etherscan');
+argv.report    && require('hardhat-gas-reporter');
 
 const settings = {
   optimizer: {
-    enabled: true,
+    enabled: argv.mode === 'production',
     runs: 999,
   },
 };
@@ -21,31 +34,41 @@ const settings = {
 module.exports = {
   solidity: {
     compilers: [
-      { version: '0.8.6',  settings },
-      { version: '0.7.6',  settings },
-      { version: '0.6.12', settings },
-      { version: '0.5.16', settings },
+      // { version: argv.compiler, settings },
+      { version: '0.8.7',       settings },
+      { version: '0.7.6',       settings },
+      { version: '0.6.12',      settings },
+      { version: '0.5.16',      settings },
     ],
   },
-  networks: {},
+  networks: {
+    hardhat: {
+      hardfork: argv.hardfork,
+    },
+  },
   etherscan: {
     apiKey: argv.etherscan,
   },
-	gasReporter: {
+  gasReporter: {
     currency: 'USD',
     coinmarketcap: argv.coinmarketcap,
   },
 };
 
+const accounts = [
+  argv.mnemonic   && { mnemonic: argv.mnemonic },
+  argv.privateKey && [ argv.privateKey ],
+].find(Boolean);
+
 Object.assign(
   module.exports.networks,
-  argv.mnemonic && Object.fromEntries([
+  accounts && Object.fromEntries([
     'mainnet',
     'ropsten',
     'rinkeby',
     'goerli',
     'kovan',
-  ].map(name => [ name, { url: argv[`${name}Node`], accounts: [ argv.mnemonic ]} ]).filter(([, { url} ]) => url)),
+  ].map(name => [ name, { url: argv[`${name}Node`], accounts } ]).filter(([, { url} ]) => url)),
   argv.slow && { hardhat: { mining: { auto: false, interval: [3000, 6000] }}}, // Simulate a slow chain locally
   argv.fork && { hardhat: { forking: { url: argv.fork }}}, // Simulate a mainnet fork
 );
