68a304773a34e0b6f6e2fcb26762720ecf88895b
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-10-10 12:03:07 +0200

    boost staking reward with pool token lock

--- contracts/finance/staking/P00lsStaking.sol
+++ contracts/finance/staking/P00lsStaking.sol
@@ -12,16 +12,18 @@ import "@amxx/hre/contracts/FullMath.sol";
 import "../amm/UniswapV2Router02.sol";
 import "../amm/libraries/Math.sol";
 
+import "hardhat/console.sol";
 
 contract P00lsStaking is AccessControl, Multicall {
     using Distributions for Distributions.Uint256;
     using Splitters     for Splitters.Splitter;
     using Timers        for Timers.Timestamp;
 
-    bytes32 public constant LOCK_MANAGER_ROLE = keccak256("LOCK_MANAGER_ROLE");
-    uint64  public constant DELAY             =      30 days;
-    uint64  public constant MIN_DURATION      = 3  * 30 days;
-    uint64  public constant MAX_DURATION      = 36 * 30 days;
+    bytes32 public  constant LOCK_MANAGER_ROLE = keccak256("LOCK_MANAGER_ROLE");
+    uint64  public  constant DELAY             =      30 days;
+    uint64  public  constant MIN_DURATION      = 3  * 30 days;
+    uint64  public  constant MAX_DURATION      = 36 * 30 days;
+    uint256 private constant EXTRA_FACTOR_BASE = 1e18; // sqrt(1e36) → double for 1 extra
 
     UniswapV2Router02 public immutable router;
     IERC20            public immutable pools;
@@ -47,8 +49,9 @@ contract P00lsStaking is AccessControl, Multicall {
      *                                                    Events                                                     *
      *****************************************************************************************************************/
     event LockSetup(IERC20 indexed token);
-    event VaultSetup(IERC20 indexed token, address indexed user, uint64 expiration);
-    event Deposit(IERC20 indexed token, address indexed user, uint256 amount, uint256 extra, uint256 newWeight);
+    event VaultSetup(IERC20 indexed token, address indexed account, uint64 expiration);
+    event Deposit(IERC20 indexed token, address indexed account, uint256 amount, uint256 extra, uint256 newWeight);
+    event Withdraw(IERC20 indexed token, address indexed account, address to, uint256 reward);
 
     /*****************************************************************************************************************
      *                                                   Modifiers                                                   *
@@ -92,8 +95,6 @@ contract P00lsStaking is AccessControl, Multicall {
         router   = _router;
         pools    = _pools;
     }
-
-
 
     function lockDetails(IERC20 token)
     public view returns (uint64 start, uint256 reward, uint256 totalWeight)
@@ -164,8 +165,6 @@ contract P00lsStaking is AccessControl, Multicall {
         }
 
         if (extra > 0) {
-            // cap extra to vault value
-            extra = Math.min(extra, router.getAmountsOut(vault.value, _tokenToPools(token))[2]);
             // deposit tokens
             SafeERC20.safeTransferFrom(pools, msg.sender, address(this), extra);
             vault.extra += extra;
@@ -173,8 +172,11 @@ contract P00lsStaking is AccessControl, Multicall {
 
         uint256 weight = _computeWeight(
             vault.value,
-            vault.extra,
-            _vaultFactor(token, to)
+            FullMath.mulDiv(
+                EXTRA_FACTOR_BASE + _extraFactor(token, vault.value, vault.extra),
+                EXTRA_FACTOR_BASE,
+                _vaultFactor(token, to)
+            )
         );
         lock.splitter._shares._balances.set(to, weight);
 
@@ -188,26 +190,22 @@ contract P00lsStaking is AccessControl, Multicall {
         Lock  storage lock  = _locks[token];
         Vault storage vault = lock.vaults[msg.sender];
 
-        uint256 value = vault.value + lock.splitter.release(msg.sender);
-        uint256 extra = vault.extra;
-
-        SafeERC20.safeTransferFrom(token, address(this), to, value);
-        SafeERC20.safeTransferFrom(pools, address(this), to, extra);
+        uint256 reward = lock.splitter.release(msg.sender);
+        SafeERC20.safeTransferFrom(token, address(this), to, vault.value + reward);
+        SafeERC20.safeTransferFrom(pools, address(this), to, vault.extra);
 
         delete lock.vaults[msg.sender];
-        // TODO emit event
+
+        emit Withdraw(token, msg.sender, to, reward);
     }
 
     /*****************************************************************************************************************
      *                                                Internal tools                                                 *
      *****************************************************************************************************************/
-
-    function _computeWeight(uint256 value, uint256 extra, uint256 factor) public view returns (uint256) {
-        // TODO: consider extra
-        // router.getAmountsOut(amount, _poolsToToken(token))[2]
+    function _computeWeight(uint256 value, uint256 factor) public pure returns (uint256) {
         return FullMath.mulDiv(
             value,
-            10 ** 18,
+            1e18,
             factor
         );
     }
@@ -215,6 +213,16 @@ contract P00lsStaking is AccessControl, Multicall {
     function _vaultFactor(IERC20 token, address account) internal view returns (uint256) {
         uint256 duration = _locks[token].vaults[account].delay.getDeadline() - _locks[token].delay.getDeadline();
         return duration * Math.sqrt(duration);
+    }
+
+    function _extraFactor(IERC20 token, uint256 value, uint256 extra) internal view returns (uint256) {
+        return extra == 0 ? 0 : Math.sqrt(
+            FullMath.mulDiv(
+                router.getAmountsOut(extra, _poolsToToken(token))[2],
+                value,
+                1e36
+            )
+        );
     }
 
     function _tokenToPools(IERC20 token) internal view returns (address[] memory path) {
@@ -226,7 +234,7 @@ contract P00lsStaking is AccessControl, Multicall {
 
     function _poolsToToken(IERC20 token) internal view returns (address[] memory path) {
         path = new address[](3);
-        path[1] = address(pools);
+        path[0] = address(pools);
         path[1] = router.WETH();
         path[2] = address(token);
     }
--- test/staking.js
+++ test/staking.js
@@ -48,7 +48,7 @@ describe('Staking', function () {
       ));
 
       // run auctions
-      await this.accounts.user.sendTransaction({ to: this.auctions[0].address, value: ethers.utils.parseEther('10') });
+      await this.accounts.user.sendTransaction({ to: this.auctions[0].address, value: ethers.utils.parseEther('1') });
       await this.accounts.user.sendTransaction({ to: this.auctions[1].address, value: ethers.utils.parseEther('1')  });
       await network.provider.send('evm_increaseTime', [ 24 * 86400 ]);
       await this.amm.auction.finalize(this.token.address);
@@ -81,7 +81,7 @@ describe('Staking', function () {
 
         const tx3 = await this.staking.connect(this.accounts.user).deposit(
           this.creatorToken.address,
-          VALUE.div(4), // value
+          VALUE.div(100), // value
           0, // no extra
           this.accounts.user.address
         );
@@ -95,14 +95,52 @@ describe('Staking', function () {
         await expect(lockDetails.start).to.be.equal(start);
         await expect(lockDetails.reward).to.be.equal(VALUE);
         await expect(vaultDetails.maturity).to.be.equal(maturity);
-        await expect(vaultDetails.value).to.be.equal(VALUE.div(4));
+        await expect(vaultDetails.value).to.be.equal(VALUE.div(100));
         await expect(vaultDetails.extra).to.be.equal(0);
         expect(lockDetails.totalWeight).to.be.equal(vaultDetails.weight);
 
-        console.log(months, vaultDetails.weight.toString())
-
+        // console.log(months, vaultDetails.weight.toString())
       });
+    }
 
+    for (const factor of new Array(10).fill().map((_, i) => i)) {
+      it(`Extra factor ${factor}`, async function () {
+        await expect(this.token.connect(this.accounts.user).approve(this.staking.address, ethers.constants.MaxUint256))
+        .to.be.not.reverted;
+        await expect(this.creatorToken.connect(this.accounts.user).approve(this.staking.address, ethers.constants.MaxUint256))
+        .to.be.not.reverted;
+
+        const tx1 = await this.staking.lockSetup(
+          this.creatorToken.address,
+        );
+
+        const tx2 = await this.staking.connect(this.accounts.user).vaultSetup(
+          this.creatorToken.address,
+          3 * 30 * 86400,
+        );
+
+        const tx3 = await this.staking.connect(this.accounts.user).deposit(
+          this.creatorToken.address,
+          VALUE.div(100),             // value
+          VALUE.div(100).mul(factor), // extra
+          this.accounts.user.address
+        );
+
+        const lockDetails  = await this.staking.lockDetails(this.creatorToken.address);
+        const vaultDetails = await this.staking.vaultDetails(this.creatorToken.address, this.accounts.user.address);
+
+        const timestamp = await ethers.provider.getBlock(tx1.blockNumber).then(({ timestamp }) => timestamp);
+        const start     = timestamp +     30 * 86400
+        const maturity  = start     + 3 * 30 * 86400
+        await expect(lockDetails.start).to.be.equal(start);
+        await expect(lockDetails.reward).to.be.equal(VALUE);
+        await expect(vaultDetails.maturity).to.be.equal(maturity);
+        await expect(vaultDetails.value).to.be.equal(VALUE.div(100));
+        await expect(vaultDetails.extra).to.be.equal(VALUE.div(100).mul(factor));
+        expect(lockDetails.totalWeight).to.be.equal(vaultDetails.weight);
+
+        // console.log(factor, vaultDetails.weight.toString())
+      });
     }
   });
 });
