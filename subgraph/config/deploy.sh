65ac577acdae8876e7b6944317d9a29e9fa7b7cd
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-05-18 17:23:51 +0200

    add live config

--- subgraph/config/deploy.sh
+++ subgraph/config/deploy.sh
@@ -2,10 +2,14 @@
 
 set -euo pipefail -x
 
-npx graph-compiler --config config/p00ls-devel.json   --include src/datasources --include node_modules/@openzeppelin/subgraphs/src/datasources --export-schema --export-subgraph
-npx graph codegen                                            ./generated/p00ls-devel.subgraph.yaml
-npx graph deploy --product hosted-service amxx/p00ls-devel   ./generated/p00ls-devel.subgraph.yaml
+npx graph-compiler --config config/p00ls.json        --include src/datasources --include node_modules/@openzeppelin/subgraphs/src/datasources --export-schema --export-subgraph
+npx graph codegen                                      ./generated/p00ls.subgraph.yaml
+npx graph deploy --product hosted-service amxx/p00ls   ./generated/p00ls.subgraph.yaml
 
-npx graph-compiler --config config/p00ls-staging.json --include src/datasources --include node_modules/@openzeppelin/subgraphs/src/datasources --export-schema --export-subgraph
-npx graph codegen                                            ./generated/p00ls-staging.subgraph.yaml
-npx graph deploy --product hosted-service amxx/p00ls-staging ./generated/p00ls-staging.subgraph.yaml
+#npx graph-compiler --config config/p00ls-devel.json   --include src/datasources --include node_modules/@openzeppelin/subgraphs/src/datasources --export-schema --export-subgraph
+#npx graph codegen                                            ./generated/p00ls-devel.subgraph.yaml
+#npx graph deploy --product hosted-service amxx/p00ls-devel   ./generated/p00ls-devel.subgraph.yaml
+
+#npx graph-compiler --config config/p00ls-staging.json --include src/datasources --include node_modules/@openzeppelin/subgraphs/src/datasources --export-schema --export-subgraph
+#npx graph codegen                                            ./generated/p00ls-staging.subgraph.yaml
+#npx graph deploy --product hosted-service amxx/p00ls-staging ./generated/p00ls-staging.subgraph.yaml
