901db1c6ce60c212fb1cc30cc97d4b499f970b02
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-06-04 16:42:15 +0200

    Remore getPair mapping

--- contracts/finance/amm/libraries/UniswapV2Library.sol
+++ contracts/finance/amm/libraries/UniswapV2Library.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0;
 
+import "@openzeppelin/contracts/proxy/Clones.sol";
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 import '../UniswapV2Factory.sol';
 
