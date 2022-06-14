c4c3ffe099d88ed844d09d1181e9eef4ae7b9460
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-06-14 21:28:54 +0200

    ERC721 refactor of the VestingFactory

--- contracts/finance/vesting/VestingFactory.sol
+++ contracts/finance/vesting/VestingFactory.sol
@@ -3,28 +3,25 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/governance/utils/IVotes.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/finance/VestingWalletUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
+import "../../utils/RegistryOwnable.sol";
 
 /// @custom:security-contact security@p00ls.com
 contract VestingTemplate is
+    RegistryOwnable,
     VestingWalletUpgradeable,
-    OwnableUpgradeable,
     MulticallUpgradeable
 {
-    constructor() initializer() {}
+    constructor(address ownershipRegistry_) initializer()
+    RegistryOwnable(ownershipRegistry_)
+    {}
 
-    function initialize(
-        address beneficiaryAddress,
-        uint64 startTimestamp,
-        uint64 durationSeconds
-    )
+    function initialize(uint64 startTimestamp, uint64 durationSeconds)
         external
         initializer()
     {
         __VestingWallet_init(address(1), startTimestamp, durationSeconds);
-        _transferOwnership(beneficiaryAddress);
     }
 
     function beneficiary()
@@ -45,16 +42,16 @@ contract VestingTemplate is
     }
 }
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/utils/Multicall.sol";
 
 /// @custom:security-contact security@p00ls.com
 contract VestingFactory is
+    ERC721("Vestings", "Vestings"),
     Multicall
 {
-    address public immutable template = address(new VestingTemplate());
-
-    event NewInstance(address instance);
+    address public immutable template = address(new VestingTemplate(address(this)));
 
     function createWallet(
         address beneficiaryAddress,
@@ -65,9 +62,17 @@ contract VestingFactory is
         returns (address)
     {
         address instance = Clones.clone(template);
-        VestingTemplate(payable(instance)).initialize(beneficiaryAddress, startTimestamp, durationSeconds);
-        emit NewInstance(instance);
+        VestingTemplate(payable(instance)).initialize(startTimestamp, durationSeconds);
+        _mint(beneficiaryAddress, addressToUint256(instance));
         return instance;
     }
 
+    function _isApprovedOrOwner(address spender, uint256 tokenId)
+        internal
+        view
+        override
+        returns (bool)
+    {
+        return addressToUint256(spender) == tokenId || super._isApprovedOrOwner(spender, tokenId);
+    }
 }
