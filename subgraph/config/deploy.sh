8b481e85ee1f627d1579679529a2e5a172cab755
Charles Couillard <charles@benderlabs.io>
2023-07-11 17:09:23 +0200

    feat: bridge production deployment

--- subgraph/config/deploy.sh
+++ subgraph/config/deploy.sh
@@ -15,3 +15,8 @@ set -euo pipefail -x
 #npx graph codegen                                            ./generated/p00ls-staging-mumbai.subgraph.yaml
 #npx graph create --node http://localhost:8020 p00ls/staging-mumbai
 #npx graph deploy p00ls/staging-mumbai ./generated/p00ls-staging-mumbai.subgraph.yaml --ipfs https://ipfs.network.thegraph.com --node http://localhost:8020 --debug
+
+#npx graph-compiler --config config/p00ls-production-polygon.json --include src/datasources --include node_modules/@openzeppelin/subgraphs/src/datasources --export-schema --export-subgraph
+#npx graph codegen                                            ./generated/p00ls-production-polygon.subgraph.yaml
+#npx graph create --node http://localhost:8020 p00ls/production-polygon
+#npx graph deploy p00ls/production-polygon ./generated/p00ls-production-polygon.subgraph.yaml --ipfs https://ipfs.network.thegraph.com --node http://localhost:8020 --debug
